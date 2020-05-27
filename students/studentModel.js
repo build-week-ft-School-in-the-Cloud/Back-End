c2eb31a12015c87260a7e73520c5e95dcd895acf
Keyeric <keyeric.bristol@gmail.com>
2020-05-27 12:07:37 -0700

    changed student Model properties to single word keys

--- students/studentModel.js
+++ students/studentModel.js
@@ -16,17 +16,17 @@ function getAll() {
       .leftJoin("volunteer as v", "v.id", "s.volunteerId")
       .select(
         //Volunteer info
-        "v.id as Volunteer ID",
-        "v.username as Teacher Username",
-        "v.forename as Teacher First Name",
-        "v.surname as Teacher Last Name",
-        "v.country as Teacher Country",
+        "v.id as VolunteerID",
+        "v.username as TeacherUsername",
+        "v.forename as TeacherFirstName",
+        "v.surname as TeacherLastName",
+        "v.country as TeacherCountry",
         //Student info
-        "s.id as Student ID",
-        "s.username as Student Username",
-        "s.forename as Student First Name",
-        "s.surname as  Student Last Name",
-        "s.country as Student Country"
+        "s.id as StudentID",
+        "s.username as StudentUsername",
+        "s.forename as StudentFirstName",
+        "s.surname as  StudentLastName",
+        "s.country as StudentCountry"
       )
       //sorted by student id number
       .orderBy("s.id")
@@ -40,15 +40,15 @@ function findByCountry(country) {
       .leftJoin("volunteer as v", "v.id", "s.volunteerId")
       .select(
         //volunteer info
-        "v.id as Volunteer ID",
-        "v.username as Teacher Username",
-        "v.forename as Teacher First Name",
-        "v.surname as Teacher Last Name",
+        "v.id as VolunteerID",
+        "v.username as TeacherUsername",
+        "v.forename as TeacherFirstName",
+        "v.surname as TeacherLastName",
         //student info
-        "s.id as Student ID",
-        "s.username as Student Username",
-        "s.forename as Student First Name",
-        "s.surname as  Student Last Name"
+        "s.id as StudentID",
+        "s.username as StudentUsername",
+        "s.forename as StudentFirstName",
+        "s.surname as  StudentLastName"
       )
       //filtered and sorted by student info
       .where("s.country", country)
@@ -68,14 +68,14 @@ function findProfile(id) {
       .join("volunteer as v", "v.id", "s.volunteerId")
       .select(
         //student info
-        "s.id as ID Number",
+        "s.id as IDNumber",
         "s.username as Username",
-        "s.forename as First Name",
-        "s.surname as Last Name",
+        "s.forename as FirstName",
+        "s.surname as LastName",
         //volunteer info
-        "v.id as Teacher ID Number",
-        "v.forename as Teacher First Name",
-        "v.surname as Teacher Last Name",
+        "v.id as TeacherIdNumber",
+        "v.forename as TeacherFirstName",
+        "v.surname as TeacherLastName",
         "s.country as Country"
       )
       //filtered by student id number
